48ae1541091ed39862f5240da1dfb361aad827ea
Antoine <mahassadijoseph@gmail.com>
2024-05-22 17:32:24 +0200

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -43,6 +43,7 @@ export default function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
 
     const toggleNav = () => setIsOpen(!isOpen);
+    const closeNav = () => setIsOpen(false);
 
 
     return (
@@ -54,19 +55,19 @@ export default function Navbar() {
                 </Link >
                 <nav className={`navLink ${isOpen ? "isActive" : ""}`} style={{ display: show ? 'flex' : 'none' }}>
                     <div className='linkQuery'>
-                        <span className='link'> <Link className='Link' to="/">home</Link> </span>
-                        <span className='link'> <Link className='Link' to="/Studio">studio</Link> </span>
-                        <span className='link'><Link className='Link' to="/Gallery">gallery</Link> </span>
-                        <span className='link'><Link className='Link' to="/Services">services</Link> </span>
-                        <span className='link'><Link className='Link' to="/Contacts">Contacts</Link></span>
+                        <span className='link' onClick={closeNav} > <Link className='Link' to="/">home</Link> </span>
+                        <span className='link' onClick={closeNav} > <Link className='Link' to="/Studio">studio</Link> </span>
+                        <span className='link' onClick={closeNav} ><Link className='Link' to="/Gallery">gallery</Link> </span>
+                        <span className='link' onClick={closeNav} ><Link className='Link' to="/Services">services</Link> </span>
+                        <span className='link' onClick={closeNav} ><Link className='Link' to="/Contacts">Contacts</Link></span>
                     </div>
                     <div className='rMobile'>
                         <div className='rMobileContent'>
                             <span className='x-2 styleF'> Glense.studio 2024</span>
                             <span className='x-1 styleF'>
-                                <Link to="/#" className='rs'> instagram</Link>
-                                <Link to="/#" className='rs'> dribble</Link>
-                                <Link to="/#" className='rs'>linkedin </Link>
+                                <Link to="/#" className='rs' onClick={closeNav} > instagram</Link>
+                                <Link to="/#" className='rs' onClick={closeNav} > dribble</Link>
+                                <Link to="/#" className='rs' onClick={closeNav} >linkedin </Link>
                             </span>
                             <span className='x-3 styleF rs'> ott, CA 9:34pm</span>
                         </div>
