c13c84dbd21718c601b11c68933e71030f6ef85c
Antoine <mahassadijoseph@gmail.com>
2024-05-22 16:03:59 +0200

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,25 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar() {
 
+
+
+    function useWindowWidth() {
+        const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+        useEffect(() => {
+            const handleResize = () => setWindowWidth(window.innerWidth);
+            window.addEventListener('resize', handleResize);
+
+            return () => window.removeEventListener('resize', handleResize);
+        }, []);
+
+        return windowWidth;
+    }
+
     const [show, handleShow] = useState(true);
     const lastScrollY = useRef(0);
 
     useEffect(() => {
-
         const showMenu = () => {
             const currentScrollY = window.scrollY;
             if (lastScrollY.current - currentScrollY > 10) {
@@ -34,13 +48,27 @@ export default function Navbar() {
                     Glense.<span className='logoS'>studio</span>
                 </Link >
                 <nav className="navLink" style={{ display: show ? 'flex' : 'none' }} >
-                    <span className='link'> <Link className='Link' to="/">home</Link> </span>
-                    <span className='link'> <Link className='Link' to="/Studio">studio</Link> </span>
-                    <span className='link'><Link className='Link' to="/Gallery">gallery</Link> </span>
-                    <span className='link'><Link className='Link' to="/Services">services</Link> </span>
-                    <span className='link'><Link className='Link' to="/Contacts">Contacts</Link></span>
+                    <div className='linkQuery'>
+                        <span className='link'> <Link className='Link' to="/">home</Link> </span>
+                        <span className='link'> <Link className='Link' to="/Studio">studio</Link> </span>
+                        <span className='link'><Link className='Link' to="/Gallery">gallery</Link> </span>
+                        <span className='link'><Link className='Link' to="/Services">services</Link> </span>
+                        <span className='link'><Link className='Link' to="/Contacts">Contacts</Link></span>
+                    </div>
+                    <div className='rMobile'>
+                        <div className='rMobileContent'>
+                            <span className='x-2 styleF'> Glense.studio 2024</span>
+                            <span className='x-1 styleF'>
+                                <Link to="/#" className='rs'> instagram</Link>
+                                <Link to="/#" className='rs'> dribble</Link>
+                                <Link to="/#" className='rs'>linkedin </Link>
+                            </span>
+                            <span className='x-3 styleF rs'> ott, CA 9:34pm</span>
+                        </div>
+                    </div>
                 </nav>
                 {!show && <div className="menu">Menu</div>}
+                <div className='menuMobile'> Menu</div>
             </div>
         </div>
 
